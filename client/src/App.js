bab761f690fad5719cbc6efefd864988947a4a59
JohnRamberger <jramberger3@gatech.edu>
2022-06-07 20:54:53 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
-import "./App.css";
-import { Controller, Scene } from "react-scrollmagic";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <Controller>
-                <Scene duration={600}>
-                    <div className="test">
-                        sdklf;sdf
-                    </div>
-                </Scene>
-            </Controller>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
