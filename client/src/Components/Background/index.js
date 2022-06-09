0bdcd55a33b7473d7d1ff5d72533bac1f96c7796
JohnRamberger <jramberger3@gatech.edu>
2022-06-09 17:07:56 -0400

    testing

---
+++ client/src/Components/Background/index.js
@@ -0,0 +1,7 @@
+//imports
+import "./Background.css"
+
+const Background = (params) => {
+    return <div className="background"></div>;
+};
+export default Background;
--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -1,18 +1,61 @@
 //imports
+import "./Home.css";
 
 import { Controller, Scene } from "react-scrollmagic";
-import { Tween } from "react-gsap";
+import { Tween, Timeline } from "react-gsap";
 
+import Background from "../../Components/Background";
 import Section from "../../Components/Section";
 import Navbar from "../../Components/Navbar";
+import { useEffect, useState } from "react";
 
 const Home = (params) => {
+    const [width, setWidth] = useState(
+        window.innerWidth > 1000 ? 1000 : window.innerWidth
+    );
+    const [height, setHeight] = useState(window.innerHeight - 5 * 16);
+    useEffect(() => {
+        window.addEventListener("resize", (e) => {
+            setWidth(e.target.innerWidth);
+            setHeight(e.target.innerHeight - 5 * 16); //5em for nav
+            if (e.target.innerWidth > 1000) {
+                setWidth(1000);
+            }
+        });
+    }, []);
+
     return (
         <div className="home-page">
+            <Background />
             <Navbar />
             <Controller>
-                <Scene indicators duration={1000} triggerHook={0} pin>
-                    <Tween
+                <Scene indicators duration="100%" triggerHook="onLeave" pin>
+                    <Timeline wrapper={<Section id="main-scene" />}>
+                        {/* <Tween from={{ y: window.innerHeight - 200 }} to={{ y: window.innerHeight / 2 - 200 }} >
+                            <div className="ground"></div>
+                        </Tween> */}
+                        <Tween
+                            from={{
+                                x: 0,
+                                y: height - 200,
+                            }}
+                            to={{ y: window.innerHeight }}
+                            position={0}
+                        >
+                            <div className="ground"></div>
+                        </Tween>
+                        <Tween
+                            from={{
+                                x: width - 100,
+                                y: height - 450,
+                            }}
+                            to={{ scale: 2 }}
+                            position={0}
+                        >
+                            <div className="telescope"></div>
+                        </Tween>
+                    </Timeline>
+                    {/* <Tween
                         wrapper={<Section />}
                         to={{
                             x: "500px",
@@ -29,9 +72,9 @@ const Home = (params) => {
                                 background: "#ccc",
                             }}
                         />
-                    </Tween>
+                    </Tween> */}
                 </Scene>
-                <Scene indicators duration={1000} triggerHook={0} pin>
+                {/* <Scene indicators duration={1000} triggerHook={0} pin>
                     <Tween
                         to={{
                             y: "500px",
@@ -49,7 +92,7 @@ const Home = (params) => {
                             }}
                         />
                     </Tween>
-                </Scene>
+                </Scene> */}
             </Controller>
         </div>
     );
