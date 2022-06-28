6ed6c8023fbc810ef927bf88faf4e3ee9c7822d3
JohnRamberger <jramberger3@gatech.edu>
2022-06-28 15:11:59 -0400

    background experiment, homepage
    created an experimental background with blurred blobs. modified testing page, created home page

--- client/src/Components/Background/index.js
+++ client/src/Components/Background/index.js
@@ -2,6 +2,12 @@
 import "./Background.css"
 
 const Background = (params) => {
-    return <div className="background"></div>;
+    return <div className="background">
+        <div className="bg-blur2"></div>
+        <div className="bg-blur"></div>
+        <div className="bg"></div>
+        <div className="circle circle-blue-bright circle1"></div>
+        <div className="circle circle-gold circle2"></div>
+    </div>;
 };
 export default Background;
--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -1,114 +1,16 @@
 //imports
 import "./Home.css";
 
-import { useEffect, useState } from "react";
-
-import { Controller, Scene } from "react-scrollmagic";
-import { Tween, Timeline } from "react-gsap";
-
 import Background from "../../Components/Background";
-import Section from "../../Components/Section";
 import Navbar from "../../Components/Navbar";
-import Faq from "../../Components/Faq";
+import Section from "../../Components/Section";
 
 const Home = (params) => {
-    const [width, setWidth] = useState(
-        window.innerWidth > 1000 ? 1000 : window.innerWidth
-    );
-    const [height, setHeight] = useState(window.innerHeight - 5 * 16);
-    useEffect(() => {
-        window.addEventListener("resize", (e) => {
-            setWidth(e.target.innerWidth);
-            setHeight(e.target.innerHeight - 5 * 16); //5em for nav
-            if (e.target.innerWidth > 1000) {
-                setWidth(1000);
-            }
-        });
-    }, []);
-
-    return (
-        <div className="home-page">
-            <Background />
-            <Navbar />
-            <Controller>
-                <Scene indicators duration="100%" triggerHook="onLeave" pin>
-                    <Timeline wrapper={<Section id="main-scene" />}>
-                        {/* <Tween from={{ y: window.innerHeight - 200 }} to={{ y: window.innerHeight / 2 - 200 }} >
-                            <div className="ground"></div>
-                        </Tween> */}
-                        <Tween
-                            from={{
-                                x: 0,
-                                y: height - 200,
-                            }}
-                            to={{ y: window.innerHeight }}
-                            position={0}
-                        >
-                            <div className="ground"></div>
-                        </Tween>
-                        <Tween
-                            from={{
-                                x: width - 100,
-                                y: height - 450,
-                            }}
-                            to={{ scale: 2 }}
-                            position={0}
-                        >
-                            <div className="telescope"></div>
-                        </Tween>
-                    </Timeline>
-                    {/* <Tween
-                        wrapper={<Section />}
-                        to={{
-                            x: "500px",
-                            rotation: 180,
-                            background: "red",
-                        }}
-                        duration={2}
-                        ease="back.out(1.7)"
-                    >
-                        <div
-                            style={{
-                                width: "100px",
-                                height: "100px",
-                                background: "#ccc",
-                            }}
-                        />
-                    </Tween> */}
-                </Scene>
-                {/* <Scene indicators duration={1000} triggerHook={0} pin>
-                    <Tween
-                        to={{
-                            y: "500px",
-                            rotation: 180,
-                            background: "blue",
-                        }}
-                        duration={2}
-                        ease="back.out(1.7)"
-                    >
-                        <div
-                            style={{
-                                width: "100px",
-                                height: "100px",
-                                background: "red",
-                            }}
-                        />
-                    </Tween>
-                </Scene> */}
-            </Controller>
-            <Section>
-                <Faq
-                    identifier="faq1"
-                    question="who can apply?"
-                    answer="Any student currently enrolled in a University above the age of 18. For any discrepancies, feel free to reach out."
-                />
-                <Faq
-                    identifier="faq2"
-                    question="who can apply?"
-                    answer="Any student currently enrolled in a University above the age of 18. For any discrepancies, feel free to reach out."
-                />
-            </Section>
-        </div>
-    );
+    return <div className="home-page">
+        <Background/>
+        <Navbar />
+        <Section />
+        <Section />
+    </div>;
 };
 export default Home;
