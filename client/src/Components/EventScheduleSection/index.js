32381d1bf1f713a68f08c9ead3e19dafde511f84
JohnRamberger <jramberger3@gatech.edu>
2023-02-03 14:28:44 -0500

    schedule

---
+++ client/src/Components/EventScheduleSection/index.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./EventScheduleSection.css";
+
+const EventScheduleSection = (params) => {
+  const [day, setDay] = useState(1);
+
+  return (
+    <div className="event-schedule-section">
+      <div className="schedule-tabs">
+        <button
+          className={`${"schedule-tabs-button"} ${day === 1 ? "_active" : ""}`}
+          onClick={() => {
+            if (day !== 1) setDay(1);
+          }}
+        >
+          Friday
+        </button>
+        <button
+          className={`${"schedule-tabs-button"} ${day === 2 ? "_active" : ""}`}
+          onClick={() => {
+            if (day !== 2) setDay(2);
+          }}
+        >
+          Saturday
+        </button>
+        <button
+          className={`${"schedule-tabs-button"} ${day === 3 ? "_active" : ""}`}
+          onClick={() => {
+            if (day !== 3) setDay(3);
+          }}
+        >
+          Sunday
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default EventScheduleSection;
